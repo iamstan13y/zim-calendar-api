92d7c8c572fb308ea2838791c61714f012c49d21
iamstan13y <iamstan13y@gmail.com>
2022-02-19 23:05:12 +0200

    Model for API.

---
+++ ZimbabweCalendar.API/Models/PublicHoliday.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZimbabweCalendar.API.Models
+{
+    public class PublicHoliday
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+    }
+}
