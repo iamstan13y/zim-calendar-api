e25cf50b9af8775484b0bbe92be72d67d99f11f2
iamstan13y <iamstan13y@gmail.com>
2022-02-20 17:00:51 +0200

    Configs for production environment.

--- ZimbabweCalendar.API/Program.cs
+++ ZimbabweCalendar.API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
